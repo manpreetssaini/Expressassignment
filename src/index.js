f239177681e30f7c2b8caa72c209244488406cc7
manpreetssaini <manpreet_saini92@hotmail.com>
2019-06-16 23:25:49 -0400

    minor changes to the imports on index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const defaultErrorHandler = require('./middleware/default-error-handle');
 const router = require('./router');
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('./static'));
-app.use('/css', express.static('./css'));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(router);
+app.use(defaultErrorHandler);
 
 const port = 3000;
 app.listen(port, () => {
